0cfbb69fed73458b871fb3860f8c7e9ca4d277f5
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-10-05 22:29:41 +0700

    belajar spring async

---
+++ src/main/java/programmerzamannow/async/BelajarSpringAsyncApplication.java
@@ -0,0 +1,15 @@
+package programmerzamannow.async;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@SpringBootApplication
+public class BelajarSpringAsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringAsyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/programmerzamannow/async/BelajarSpringAsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package programmerzamannow.async;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringAsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
