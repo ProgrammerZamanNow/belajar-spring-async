7c81d3e8cd413c4a9b7e30d61d40c9d2eb47b018
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-10-05 23:14:03 +0700

    custom scheduled executor

--- src/main/java/programmerzamannow/async/AsyncConfiguration.java
+++ src/main/java/programmerzamannow/async/AsyncConfiguration.java
@@ -5,9 +5,15 @@ import org.springframework.context.annotation.Configuration;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 @Configuration
 public class AsyncConfiguration {
+
+  @Bean
+  public ScheduledExecutorService taskScheduler(){
+    return Executors.newScheduledThreadPool(10);
+  }
 
   @Bean
   public Executor taskExecutor(){
